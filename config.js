91362e0c1a7e306883a65354043e9d664857ee23
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-08 15:58:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "put sesion_id",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c8858afdce80eeb01b47e.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "👋 Hello *${pushname}* 🌝👋 HELLO, I AM NIRO-MD I AM ALIVE NOW !",
+ALIVE_MSG: process.env.ALIVE_IMG || "👋 Hello *${pushname}*\n🌝👋 HELLO, I AM NIRO-MD I AM ALIVE NOW !",
 SUDO_NB: process.env.SUDO_NB || "94762296665",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
