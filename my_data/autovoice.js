78c050ad431fadf22094cd3c4bd0342d96dee21c
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-10 22:11:03 +0530

    Update autovoice.js

--- my_data/autovoice.js
+++ my_data/autovoice.js
@@ -1,4 +1,4 @@
 {
-"Hurathal wenna epa":""
-"Ane":""
+"Hurathal wenna epa":"https://github.com/Navinofc44/Niro-Md-Voice/raw/main/autovoice/AUD-20240910-WA0256.opus"
+"Ane":"https://github.com/Navinofc44/Niro-Md-Voice/raw/main/autovoice/AUD-20240910-WA0257.opus"
 }
