d342f1f867f59e7e6e7ed988017f54077ecabfab
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-11 20:14:23 +0530

    Delete my_data directory

--- my_data/autovoice.js
+++
@@ -1,4 +0,0 @@
-{
-"Hurathal wenna epa":"https://github.com/Navinofc44/Niro-Md-Voice/raw/main/autovoice/AUD-20240910-WA0256.opus"
-"Ane":"https://github.com/Navinofc44/Niro-Md-Voice/raw/main/autovoice/AUD-20240910-WA0257.opus"
-}
